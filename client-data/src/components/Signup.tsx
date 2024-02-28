7a9376896eeb83195e4a794f67823e38e594d138
OU5AMA <lasri.oussama@gmail.com>
2024-02-28 13:47:07 +0100

    fetch for the backend and response the userData

--- client-data/src/components/Signup.tsx
+++ client-data/src/components/Signup.tsx
@@ -2,90 +2,113 @@ import { useState } from "react";
 import "./signup.css";
 
 const Signup = () => {
-    const [userData, setUserData] = useState({
-        username: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({
+      ...userData,
+      [name]: value,
     });
+  };
 
-    const [errors, setErrors] = useState({});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setUserData({
-            ...userData,
-            [name]: value
+    // Validation rules
+    const newErrors = {};
+
+    // Validating username
+    if (userData.username.trim().length < 3 || userData.username.trim().length > 50 ) {
+      newErrors.username = "Username must be between 3 and 50 charcaters long";
+    }else if(!userData.username.trim().match(/^[a-zA-Z0-9_]+$/)){
+        newErrors.username = "Username must contain letters, numbers, and underscores only";
+    }
+
+    // Validating email
+    if (!userData.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!userData.email.trim().match(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/)) {
+      newErrors.email = "Choose a valid email";
+    }
+
+
+    if (!userData.password.trim() < 8) {
+      newErrors.password = "Password must be at least 8 characters long";
+    }
+
+    if (userData.confirmPassword.trim() !== userData.password.trim()) {
+      newErrors.confirmPassword = "Your passwords do not match!";
+    }
+    setErrors(newErrors);
+
+    // If there are no errors you can proceed with form submission
+    if (Object.keys(newErrors).length === 0) {
+      // handle form submission here
+      try {
+        const response = await fetch("http://localhost:8080/signup", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userData)
         });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newErrors = {};
-        if (!userData.username.trim()) {
-            newErrors.username = "Username is required";
-        }
-
-        if (!userData.email.trim()) {
-            newErrors.email = "Email is required";
-        } else if (!userData.email.includes('@')) {
-            newErrors.email = "Choose a valid email";
-        }
-
-        if (!userData.password.trim()) {
-            newErrors.password = "Password is required";
-        }
-
-        if (userData.confirmPassword !== userData.password) {
-            newErrors.confirmPassword = "Your password doesn't match!";
-        }
-        setErrors(newErrors);
-        
-
-        // If there are no errors you can proceed with form submission
-        if (Object.keys(newErrors).length === 0) {
-            // handle form submission here
-            console.log('form submitted', userData);
+        if(response.ok){
+            console.log("User signed up successfully! 😎😉");
         }else{
-            console.log(errors, newErrors);
+            console.error("Failed to signup user. 😒")
         }
-    };
+      } catch (error) {
+        console.error("Error: ", errors);
+      }
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>Username: </label>
-            <input
-                type="text"
-                name="username"
-                value={userData.username}
-                onChange={handleChange}
-            />
-            <label>Email: </label>
-            <input
-                type="email"
-                name="email"
-                value={userData.email}
-                onChange={handleChange}
-            />
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Username: </label>
+      <input
+        type="text"
+        name="username"
+        value={userData.username}
+        onChange={handleChange}
+      />
+      
 
-            <label>Password: </label>
-            <input
-                type="password"
-                name="password"
-                value={userData.password}
-                onChange={handleChange}
-            />
+      <label>Email: </label>
+      <input
+        type="email"
+        name="email"
+        value={userData.email}
+        onChange={handleChange}
+      />
+      
 
-            <label>Confirm Password: </label>
-            <input
-                type="password"
-                name="confirmPassword"
-                value={userData.confirmPassword}
-                onChange={handleChange}
-            />
-            <button type="submit">Sign Up</button>
-        </form>
-    );
+      <label>Password: </label>
+      <input
+        type="password"
+        name="password"
+        value={userData.password}
+        onChange={handleChange}
+      />
+
+      <label>Confirm Password: </label>
+      <input
+        type="password"
+        name="confirmPassword"
+        value={userData.confirmPassword}
+        onChange={handleChange}
+      />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
 };
 
 export default Signup;
